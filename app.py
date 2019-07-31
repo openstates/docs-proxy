06f146908f8d67919441a663b818f192a641baa8
Tim Showers <showerst@gmail.com>
2019-07-31 10:55:58 -0400

    Change user agent to browser to stop cloudflare blocks

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def get_doc(doc_link):
     headers = {}
     headers['Authorization'] = os.environ['INDIANA_API_KEY']
     headers['Content-Type'] = "application/pdf"
-    headers['User-Agent'] = 'openstates-in-proxy'
+    headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'
     full_link = "https://api.iga.in.gov/" + doc_link + "?format=pdf"
     page = requests.get(full_link,headers=headers,verify=False)
 
